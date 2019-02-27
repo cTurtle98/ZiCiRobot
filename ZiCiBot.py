31bacc4e52918f51c86164dbec23b3158f4f5117
cTurtle98 <ciaran@cturtle98.com>
2019-02-26 17:03:49 -0800

    main code

---
+++ ZiCiBot.py
@@ -0,0 +1,22 @@
+'''
+Ciaran Farley
+Ziah Jyothi
+
+ZiCiRobot Control
+
+Version 0.1
+
+This program takes input from HID joysticks and maps it to https://www.adafruit.com/product/815 for controlling a robot
+
+We are using:
+https://www.pygame.org/docs/ref/joystick.html
+https://github.com/adafruit/Adafruit_CircuitPython_ServoKit
+'''
+
+import pygame
+
+pygame.joystick.init()
+
+
+
+
