1b8a8e53e6e731bcb9e50949c6bb50510b7ba434
cTurtle98 <ciaran@cturtle98.com>
2019-03-02 22:55:44 -0800

    debug

--- joytest.py
+++ joytest.py
@@ -11,6 +11,8 @@ pygame.joystick.init()
 
 joystick_count = pygame.joystick.get_count()
 
+pygame.event.get()
+
 for i in range(joystick_count):
   joystick = pygame.joystick.Joystick(i)
   joystick.init()
