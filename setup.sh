fe916100bc4ace94947b7bf0207300fa98f8021d
cTurtle98 <ciaran@cturtle98.com>
2019-03-02 21:23:48 -0800

    setup script

---
+++ setup.sh
@@ -0,0 +1,10 @@
+#
+# ZiCi-Robot
+# https://github.com/cTurtle98/ZiCiRobot/blob/master/ZiCiBot.py
+# 
+# setup script for preparing a raspberry pi for being the robot brain
+#
+
+sudo apt install python3-pip
+
+sudo pip3 install pygame adafruit-circuitpython-servokit
