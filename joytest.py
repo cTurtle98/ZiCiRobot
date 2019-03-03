0e386ab91d071a12e8603b3200080a632abf5889
cTurtle98 <ciaran@cturtle98.com>
2019-03-02 22:51:13 -0800

    debug

--- joytest.py
+++ joytest.py
@@ -16,19 +16,19 @@ for i in range(joystick_count):
   joystick.init()
   
   name = joystick.get_name()
-  print("Joystick name: " + name)
+  print("Joystick name: " name)
   
   axes = joystick.get_numaxes()
   
   for i in range( axes ):
     axis = joystick.get_axis( i )
-    print("Axis ", + i, + " value: ", axis)
+    print("Axis ", i, " value: ", axis)
   
   buttons = joystick.get_numbuttons()
   
   for i in range( buttons ):
     button = joystick.get_button( i )
-    print ("button ", + i, + "value ", + button)
+    print ("button ", i, "value ", button)
     
   print()
   print()
