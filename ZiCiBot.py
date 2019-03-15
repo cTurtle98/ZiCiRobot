185146fd3893b8a7b847d4652216c7a4f91f1a9c
cTurtle98 <ciaran@cturtle98.com>
2019-03-14 23:12:48 -0700

    starting to write an arm mode

--- ZiCiBot.py
+++ ZiCiBot.py
@@ -42,30 +42,56 @@ stearingTrim = 27
 #center the throttle
 throttleAngle = 90
 
+# control mode
+mode = "1"
+MAXMODE = 2
+
 # main while loop to keep the program running till I kill it
 while True:
     # instanciate an object of the event manager from the inputs library
     ds4_events = inputs.get_gamepad()
     # for loop to let me handle each event individually
     for ds4 in ds4_events:
-        # if my event has a code of ABS_X (left stick x axis) run next code
-        if ds4.code == "ABS_X" :
-            # set the stearing angle to be the state (value) of the event mapped from a byte size to a degree then add the trim (see comments above)
-            stearingAngle = int(map_value(ds4.state, 0, 255, 0, 128)) + stearingTrim
-            # publish stearing angle to the pwm driver board
-            # stearing servo is on channel 0
-            pwm.servo[0].angle = stearingAngle
-            
-            
-        # if the event has a code of ABS_RZ (R2 Axis)
-        # right trigger is forward (90 to 128 degrees) on the speed controller
-        if ds4.code ==  "ABS_RZ" :
-            # map the value from L2 into the forward range for the speed controller for throttle
-            throttleAngle = int(map_value(ds4.state, 0, 255, 90, 128))
-        # else if the event has a code of ABS_Z (L2 axis)
-        # left trigger is reverse (90 to 0 degrees) on the speed controller
-        elif ds4.code == "ABS_Z" :
-            # map the value from the L2 trigger to 90 through 0 degrees on the speed controller
-            throttleAngle = int(map_value(ds4.state, 0, 255, 90, 0))
-        pwm.servo[1].angle = throttleAngle
-        
+
+        '''
+        ################################
+        # MODE CHANGE
+        if ds4.code == "BTN_TR" :
+            if mode < 0 and mode <= MAXMODE :
+                mode++
+            if mode = MAXMODE :
+                mode = 1
+        if ds4.code == "BTN_TL" :
+            if mode < 0 and mode <= MAXMODE :
+                mode--
+            if mode = 1 :
+                mode = MAXMODE
+        '''
+
+        ###############################
+        #DRIVING MODE
+        if mode == "1" :
+            # if my event has a code of ABS_X (left stick x axis) run next code
+            if ds4.code == "ABS_X" :
+                # set the stearing angle to be the state (value) of the event mapped from a byte size to a degree then add the trim (see comments above)
+               stearingAngle = int(map_value(ds4.state, 0, 255, 0, 128)) + stearingTrim
+               # publish stearing angle to the pwm driver board
+               # stearing servo is on channel 0
+               pwm.servo[0].angle = stearingAngle
+                        
+            # if the event has a code of ABS_RZ (R2 Axis)
+            # right trigger is forward (90 to 128 degrees) on the speed controller
+            if ds4.code ==  "ABS_RZ" :
+                # map the value from L2 into the forward range for the speed controller for throttle
+                throttleAngle = int(map_value(ds4.state, 0, 255, 90, 128))
+            # else if the event has a code of ABS_Z (L2 axis)
+            # left trigger is reverse (90 to 0 degrees) on the speed controller
+            elif ds4.code == "ABS_Z" :
+                # map the value from the L2 trigger to 90 through 0 degrees on the speed controller
+                throttleAngle = int(map_value(ds4.state, 0, 255, 90, 0))
+            pwm.servo[1].angle = throttleAngle
+
+        ###############################
+        # ARM MODE
+        if mode == "2" :
+            if ds4.code == "" :
